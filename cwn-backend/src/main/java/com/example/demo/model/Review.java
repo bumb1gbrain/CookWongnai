a06f1e4d12570ca2b70e84a6ebc309a3c644b4af
Museey <musee.hyper@gmail.com>
2024-09-15 21:12:57 +0700

    no more next

--- cwn-backend/src/main/java/com/example/demo/model/Review.java
+++ cwn-backend/src/main/java/com/example/demo/model/Review.java
@@ -4,20 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Review {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String comment;
-    private int rating;
+    private Integer rating;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
+    @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
     public Long getId() {
